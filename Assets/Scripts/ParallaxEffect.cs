5988472fe4708a3bb145418110c407a10d91e4e0
Daniel <danieruone@gmail.com>
2020-03-29 16:57:06 -0500

    first commit

---
+++ Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour{
+    
+    private float lenght, startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera");
+        startpos = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + lenght) startpos += lenght;
+        else if (temp < startpos - lenght) startpos -= lenght;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public PlayerController controller;
+    public float runSpeed = 30f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+            jump = true;
+            GetComponent<Animator>().SetTrigger("Jump");
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(runSpeed * Time.deltaTime, false, jump);
+        GetComponent<Animator>().SetBool("isRunning", true);
+        jump = false;
+    }
+}
